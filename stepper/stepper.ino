ce00a5444cadb402b7da0567676ee876d1a9950f
gruvw <gruvw.dev@gmail.com>
2024-02-29 10:35:02 +0100

    Started to code origin

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,25 +1,57 @@
-#define STEPPER 4
-#define STEP_DELAY 2000
+#define STEPS 4
+#define STEP_DELAY delayMicroseconds(2000)
+#define LIMIT_DIST 3000
 
-const int STEP_X[STEPPER] = {DD5, DD4, DD3, DD2};
-const int STEP_Y[STEPPER] = {A0, A1, A2, A3};
+typedef struct {
+    const int STEP[STEPS];
+    unsigned steps;
+} Stepper;
 
-unsigned steps_X = 0;
-unsigned steps_Y = 0;
+typedef struct {
+    unsigned x;
+    unsigned y;
+} Pos2D;
+
+typedef struct {
+    Stepper X;
+    Stepper Y;
+    Pos2D pos;
+} Axes2D;
+
+Stepper X = {DD5, DD4, DD3, DD2};
+Stepper Y = {A0, A1, A2, A3};
+Axes2D axes = {X, Y};
 
 const int limit_switch = DD7;
 
-void setup() {
-}
-
-void step(bool forward) {
-    steps_X = (steps_X + (forward ? 1 : -1)) % STEPPER;
-    for (int i = 0; i < STEPPER; i++) {
-        digitalWrite(STEP_X[i], (steps_X - i) == 0 ? HIGH : LOW);
+void step(Stepper * stepper, bool forward) {
+    stepper->steps = (stepper->steps + (forward ? 1 : -1)) % STEPS;
+    for (int i = 0; i < STEPS; i++) {
+        digitalWrite(stepper->STEP[i], (stepper->steps - i) == 0 ? HIGH : LOW);
     }
 }
 
+void origin_axis(Stepper * stepper) {
+    while (!digitalRead(limit_switch)) {
+        step(stepper, false);
+        STEP_DELAY;
+    }
+
+    for (unsigned i = 0; i < LIMIT_DIST; ++i) {
+        step(stepper, true);
+        STEP_DELAY;
+    }
+}
+
+void origin(Axes2D * axes) {
+    origin_axis(&axes->X);
+    origin_axis(&axes->Y);
+    axes->pos = {0, 0};
+}
+
+void setup() {
+    origin(&axes);
+}
+
 void loop() {
-    delayMicroseconds(STEP_DELAY);
-    step(true);
 }
